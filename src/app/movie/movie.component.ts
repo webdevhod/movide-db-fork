1eff9deb947fab72dd4e5d20f48acb3a4c7774ec
DESKTOP-INTEL\yschr <yuensang.christ@gmail.com>
2023-10-22 19:54:35 -0700

    Fixed Movie page images path and added movie route

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
   styleUrls: ['./movie.component.scss'],
 })
-export class MovieComponent {}
+export class MovieComponent {
+// public id: string;
+constructor(private route: ActivatedRoute) {}
+
+ngOnInit() {
+  //  this.id = this.route.snapshot.paramMap.get('movieId');
+  console.log(this.route.snapshot.paramMap.get('movieId'));
+}
+
+}
+
+
 
 
