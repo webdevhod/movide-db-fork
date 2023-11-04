405ba0fab7bcfdd40b837160cc3870e05c4b175c
DESKTOP-INTEL\yschr <yuensang.christ@gmail.com>
2023-11-03 20:55:08 -0700

    completed search movie and movie search result

--- src/app/movie-search-result/movie-search-result.component.ts
+++ src/app/movie-search-result/movie-search-result.component.ts
@@ -4,6 +4,7 @@ import { DiscoverMovie } from '../interfaces/discover-movie.interface';
 import { TmdbService } from '../services/tmdb.service';
 import { ActivatedRoute, ParamMap, Params } from '@angular/router';
 import { Observable, lastValueFrom, map } from 'rxjs';
+import { DiscoverMovieResult } from '../interfaces/discover-movieresult.interface';
 
 @Component({
   selector: 'app-movie-search-result',
@@ -11,34 +12,29 @@ import { Observable, lastValueFrom, map } from 'rxjs';
   styleUrls: ['./movie-search-result.component.scss'],
 })
 export class MovieSearchResultComponent {
-  searchValue: string | undefined | null;
-  searchType: string | undefined | null;
+  searchValue: string = '';
+  searchType: string = '';
   movieSearchResult: DiscoverMovie | undefined;
+  movieResults: DiscoverMovieResult[] | undefined;
 
   constructor(private route: ActivatedRoute, public tmdb: TmdbService) {}
 
   async ngOnInit() {
-    // this.route.queryParams.subscribe((queryParams) => {
-    //   this.searchValue = queryParams['value'];
-    //   this.searchType = queryParams['type'];
-    //   console.log(this.searchValue);
-    //   console.log(this.searchType);
-    // });
+    this.route.queryParams.subscribe((queryParams) => {
+      this.searchValue = queryParams['value'];
+      this.searchType = queryParams['type'];
+      console.log(this.searchValue);
+      console.log(this.searchType);
 
-    this.searchValue = this.route.snapshot.queryParamMap.get('value');
-    if (this.searchValue === null || this.searchValue === undefined) {
-      throw new Error('Search value not found');
-    }
-    this.searchType = this.route.snapshot.queryParamMap.get('type');
-    if (this.searchType === null || this.searchType === undefined) {
-      throw new Error('Search type not found');
-    }
-
-    if (this.searchType == 'movie') {
-      this.movieSearchResult = await lastValueFrom(
-        this.tmdb.searchMovieFromTitle(this.searchValue)
-      );
-      console.log(this.movieSearchResult);
-    }
+      if (this.searchType == 'movie') {
+        lastValueFrom(this.tmdb.searchMovieFromTitle(this.searchValue)).then(
+          (result: DiscoverMovie | undefined) => {
+            console.log(result);
+            this.movieSearchResult = result;
+          }
+        );
+        this.movieResults = this.movieSearchResult?.results;
+      }
+    });
   }
 }
