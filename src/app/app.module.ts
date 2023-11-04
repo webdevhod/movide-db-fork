06558aa43e74178c522c8c92fde814bf9ded73b8
Daniel Ho <webdevhod@gmail.com>
2023-11-03 23:36:52 -0700

    Added movie search pagination.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DropdownModule } from 'primeng/dropdown';
 import { InputTextModule } from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [
@@ -37,6 +38,7 @@ import { InputTextModule } from 'primeng/inputtext';
     DropdownModule,
     FormsModule,
     InputTextModule,
+    TableModule,
   ],
   providers: [DatePipe, RuntimeConverterPipe],
   bootstrap: [AppComponent],
